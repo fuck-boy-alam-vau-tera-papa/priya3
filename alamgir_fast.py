845e0eab86693fcf9abafecc86eda26f7d69d94c
Md. Alamgir Hosen <64268610+fuck-boy-alam-vau-tera-papa@users.noreply.github.com>
2022-07-12 11:21:13 +0600

    Add files via upload

---
+++ alamgir_fast.py
@@ -0,0 +1,7 @@
+# Encrypted by Hamid Meer
+# Whatsapp 03155912839 
+# Github- https://github.com/Hamii-king-06
+
+exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
+""
+.split("  ")])))
